c3b74cda14ccbb185aea3d9c685cdc022fa44657
Lexi Kronowitz <lexikronowitz@lexis-air.wifi.local.cmu.edu>
2024-10-03 22:30:49 -0400

    Updated controlelr file

--- src/controllers/additional-info.js
+++ src/controllers/additional-info.js
@@ -1,37 +1,18 @@
 'use strict';
 
 const nconf = require('nconf');
-const validator = require('validator');
-
 const helpers = require('./helpers');
-const recentController = require('./recent');
 
 const additionalInfoController = module.exports;
 
 additionalInfoController.get = async function (req, res, next) {
-	const data = await recentController.getData(req, 'additional-info', 'posts');
-	if (!data) {
-		return next();
-	}
-	const term = helpers.terms[req.query.term] || 'alltime';
-	if (req.originalUrl.startsWith(`${nconf.get('relative_path')}/api/additional-info`) || req.originalUrl.startsWith(`${nconf.get('relative_path')}/additional-info`)) {
-		data.title = `[[pages:additional-info-${term}]]`;
-		const breadcrumbs = [{ text: '[[global:header.additional-info]]' }];
-		data.breadcrumbs = helpers.buildBreadcrumbs(breadcrumbs);
-	}
+    // You can customize the data being sent to the template
+    const data = {
+        title: 'Additional Information',
+        breadcrumbs: helpers.buildBreadcrumbs([{ text: 'Additional Information' }]),
+        // Add any other static or dynamic data you want to display
+    };
 
-	if (!data['feeds:disableRSS'] && data.rssFeedUrl) {
-		const feedQs = data.rssFeedUrl.split('?')[1];
-		data.rssFeedUrl = `${nconf.get('relative_path')}/additional-info/${validator.escape(String(req.query.term || 'alltime'))}.rss`;
-		if (req.loggedIn) {
-			data.rssFeedUrl += `?${feedQs}`;
-		}
-	}
-	res.render('additional-info', data);
+    // Render the additional-info template without the 'templates/' prefix
+    res.render('additional-info', data);
 };
-
-
-
-
-
-
